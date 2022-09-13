49db505715eab0226fb7c4eda9346ed8a90be724
celines <celina.alsina@gmail.com>
2022-09-13 16:03:04 +0200

    upgrade amplify auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -8,6 +8,9 @@ export type AmplifyDependentResourcesAttributes = {
             "UserPoolName": "string",
             "AppClientIDWeb": "string",
             "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "tdcfe361abbuserpoolfe361abbdevGroupRole": "string"
         }
     }
 }
