d76bf669b964da5f74d495c76ac257a4867e98e7
celines <celina.alsina@gmail.com>
2022-09-13 16:21:02 +0200

    noentiendonada

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
